6054472f96d3e72e4e0ef08c9f5fbc729a44f9ba
Joshua Krinsky <JoshuaPKrinsky@gmail.com>
2018-10-19 12:20:38 -0400

    got camera working now have to findout how to save an image

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -2,35 +2,43 @@ import React, {Component} from 'react';
 import {Text,View,Image,TouchableWithoutFeedback,} from 'react-native';
 import Ionicon from 'react-native-vector-icons/Ionicons';
 
-export default class Photo extends Component
-{
-    render()
-    {
-        return(
-            <View>
+export default class Photo extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <View style={styles.conatiner}>
                 <View>
-                    <Image />
+                    <Image/>
                     <Text>userName</Text>
                 </View>
-            <View>
-                <Image />
-            </View>
-            <View>
-                <TouchableWithoutFeedback>
-                    <Ionicon name = "aperture"
-                    size = {30}/>
-                </TouchableWithoutFeedback>
-            </View>
-            <View>
-                <Text>
-                    userName
-                </Text>
-                <Text>
-                    Comment
-                </Text>
-            </View>
+                <View>
+                    <Image/>
+                </View>
+                <View>
+                    <TouchableWithoutFeedback>
+                        <Ionicon name="aperture"
+                                 size={30}/>
+                    </TouchableWithoutFeedback>
+                </View>
+                <View>
+                    <Text>
+                        userName
+                    </Text>
+                    <Text>
+                        Comment
+                    </Text>
+                </View>
             </View>
 
-    );
+        );
     }
+}
+    const styles ={
+        container: {
+            backgroundColor: '#ffffff',
+            padding: 10
+        }
 }
